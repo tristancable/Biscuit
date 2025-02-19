c2466374adff57a655dbe35fc2cdd04db5b23b01
ezeklaww <eandreason@student.neumont.edu>
2025-02-19 09:17:08 -0700

    changed to a .NET MAUI instead of the WPF

--- DigitRecognizer/App.xaml.cs
+++ DigitRecognizer/App.xaml.cs
@@ -1,14 +1,12 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
-
-namespace DigitRecognizer
+﻿namespace DigitRecognizer
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
-    }
+        public App()
+        {
+            InitializeComponent();
 
+            MainPage = new AppShell();
+        }
+    }
 }
