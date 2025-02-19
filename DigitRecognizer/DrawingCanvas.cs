214d717509fa618ee62cfb7a729eeb083d552bec
tristancable <tcable@student.neumont.edu>
2025-02-19 11:37:27 -0700

    Canvas to draw on

---
+++ DigitRecognizer/DrawingCanvas.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Maui.Graphics;
+
+public class DrawingCanvas : IDrawable
+{
+    private List<PointF> _points = new List<PointF>();
+
+    public void AddPoint(PointF point)
+    {
+        _points.Add(point);
+    }
+
+    public void Clear()
+    {
+        _points.Clear();
+    }
+
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        // Set stroke properties (color, width)
+        canvas.StrokeColor = Colors.Black;
+        canvas.StrokeSize = 2;
+
+        // Draw each point as a small line segment
+        if (_points.Count > 1)
+        {
+            for (int i = 0; i < _points.Count - 1; i++)
+            {
+                canvas.DrawLine(_points[i], _points[i + 1]);
+            }
+        }
+    }
+}
--- DigitRecognizer/MainPage.xaml.cs
+++ DigitRecognizer/MainPage.xaml.cs
@@ -1,14 +1,55 @@
-﻿namespace DigitRecognizer
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using System;
+
+namespace DigitRecognizer
 {
     public partial class MainPage : ContentPage
     {
-
+        public static DrawingCanvas DrawableInstance = new DrawingCanvas();
 
         public MainPage()
         {
             InitializeComponent();
+            CanvasView.Drawable = DrawableInstance;
+
+            // Subscribe to the interaction events for the canvas
+            CanvasView.StartInteraction += OnCanvasPointerPressed;
+            CanvasView.DragInteraction += OnCanvasPointerMoved;
+            CanvasView.EndInteraction += OnCanvasPointerReleased;
         }
 
-    }
+        private void OnCanvasPointerPressed(object sender, TouchEventArgs e)
+        {
+            // Handle touch start
+            var touchPoint = e.Touches[0];
+            if (touchPoint != null)
+            {
+                DrawableInstance.AddPoint(new PointF((float)touchPoint.X, (float)touchPoint.Y));
+                CanvasView.Invalidate(); // Refresh the drawing
+            }
+        }
 
+        private void OnCanvasPointerMoved(object sender, TouchEventArgs e)
+        {
+            // Handle touch move
+            var touchPoint = e.Touches[0];
+            if (touchPoint != null)
+            {
+                DrawableInstance.AddPoint(new PointF((float)touchPoint.X, (float)touchPoint.Y));
+                CanvasView.Invalidate(); // Update the drawing as user moves finger
+            }
+        }
+
+        private void OnCanvasPointerReleased(object sender, TouchEventArgs e)
+        {
+            // Optionally handle pointer release here if needed
+        }
+
+        private void OnClearClicked(object sender, EventArgs e)
+        {
+            DrawableInstance.Clear();
+            CanvasView.Invalidate(); // Clear the canvas
+        }
+    }
 }
