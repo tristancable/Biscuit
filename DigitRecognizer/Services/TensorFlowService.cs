2659aa3b88ddd2d01f30e46d6139603b58adee2d
Xenny76 <dredford@student.neumont.edu>
2025-02-20 10:24:55 -0700

    Added test data

---
+++ DigitRecognizer/Services/TensorFlowService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NumSharp;
+using Tensorflow;
+namespace DigitRecognizer.Services
+{
+    public static class TensorFlowService
+    {
+        static int img_h = 28;
+        static int img_w = 28;
+        static int img_size_flat = img_w * img_h;
+        static int n_classes = 10;
+        static int n_channels = 1;
+        public static void PrepareData()
+        {
+        }
+    }
+}
