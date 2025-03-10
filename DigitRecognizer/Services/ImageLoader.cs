e20e4d3ed82aa8d560840f20bc085c3ea1c35577
ezeklaww <eandreason@student.neumont.edu>
2025-03-09 21:58:22 -0600

    Changes 2D thing to be reading it by a Skia bitmap. Should work, can't visualize it though.

--- DigitRecognizer/Services/ImageLoader.cs
+++ DigitRecognizer/Services/ImageLoader.cs
@@ -7,93 +7,85 @@ namespace DigitRecognizer.Services
         int imageSize = 28;
         bool greyscale = true;
         DataFile[] dataFiles;
-        string[] labelNames;
-        Image[] images;
+        string[] labelNames = Array.Empty<string>();
+        Image[] images = Array.Empty<Image>();
 
-        public int NumImages => images.Length;
+        public int NumImages => images?.Length ?? 0;
         public int InputSize => imageSize * imageSize * (greyscale ? 1 : 3);
         public int OutputSize => labelNames.Length;
         public string[] LabelNames => labelNames;
 
-        void Awake()
+        // imageFilePath = "MNIST_TestData/t10k-images-idx3-ubyte", labelFilePath = "MNIST_TestData/t10k-labels-idx1-ubyte"
+
+        public ImageLoader(DataFile[] files)
         {
+            dataFiles = files ?? throw new ArgumentNullException(nameof(files));
             images = LoadImages();
         }
 
         public Image GetImage(int i)
         {
+            if (images.Length == 0) throw new InvalidOperationException("No images have been loaded.");
+            if (i < 0 || i >= images.Length) throw new IndexOutOfRangeException("Image index out of range.");
             return images[i];
         }
 
         public DataPoint[] GetAllData()
         {
-            DataPoint[] allData = new DataPoint[images.Length];
-            for (int i = 0; i < allData.Length; i++)
-            {
-                allData[i] = DataFromImage(images[i]);
-            }
-            return allData;
+            return images.Select(DataFromImage).ToArray();
         }
 
-        DataPoint DataFromImage(Image image)
+        private DataPoint DataFromImage(Image image)
         {
             return new DataPoint(image.pixelValues, image.label, OutputSize);
         }
 
-        Image[] LoadImages()
+        private Image[] LoadImages()
         {
             List<Image> allImages = new List<Image>();
 
             foreach (var file in dataFiles)
             {
+                if (!File.Exists(file.imageFilePath) || !File.Exists(file.labelFilePath))
+                    throw new FileNotFoundException($"File not found: {file.imageFilePath} or {file.labelFilePath}");
+
                 byte[] imageData = File.ReadAllBytes(file.imageFilePath);
                 byte[] labelData = File.ReadAllBytes(file.labelFilePath);
-                Image[] images = LoadImages(imageData, labelData);
-                allImages.AddRange(images);
+                allImages.AddRange(LoadImages(imageData, labelData));
             }
 
             return allImages.ToArray();
-
-
-            Image[] LoadImages(byte[] imageData, byte[] labelData)
-            {
-                int numChannels = (greyscale) ? 1 : 3;
-                int bytesPerImage = imageSize * imageSize * numChannels;
-                int bytesPerLabel = 1;
-
-                int numImages = imageData.Length / bytesPerImage;
-                int numLabels = labelData.Length / bytesPerLabel;
-                Debug.Assert(numImages == numLabels, $"Number of images doesn't match number of labels ({numImages} / {numLabels})");
-
-                int dataSetSize = System.Math.Min(numImages, numLabels);
-                var images = new Image[dataSetSize];
-
-                // Scale pixel values from [0, 255] to [0, 1]
-                double pixelRangeScale = 1 / 255.0;
-                double[] allPixelValues = new double[imageData.Length];
-
-                System.Threading.Tasks.Parallel.For(0, imageData.Length, (i) =>
-                {
-                    allPixelValues[i] = imageData[i] * pixelRangeScale;
-                });
-
-                // Create images
-                System.Threading.Tasks.Parallel.For(0, numImages, (imageIndex) =>
-                {
-                    int byteOffset = imageIndex * bytesPerImage;
-                    double[] pixelValues = new double[bytesPerImage];
-                    System.Array.Copy(allPixelValues, byteOffset, pixelValues, 0, bytesPerImage);
-                    Image image = new Image(imageSize, greyscale, pixelValues, labelData[imageIndex]);
-                    images[imageIndex] = image;
-                });
-
-                return images;
-            }
-
-
         }
 
-        [System.Serializable]
+        private Image[] LoadImages(byte[] imageData, byte[] labelData)
+        {
+            int numChannels = greyscale ? 1 : 3;
+            int bytesPerImage = imageSize * imageSize * numChannels;
+            int numImages = imageData.Length / bytesPerImage;
+            int numLabels = labelData.Length;
+
+            if (numImages != numLabels)
+                throw new InvalidOperationException($"Number of images ({numImages}) doesn't match number of labels ({numLabels}).");
+
+            int dataSetSize = Math.Min(numImages, numLabels);
+            var images = new Image[dataSetSize];
+
+            double[] allPixelValues = new double[imageData.Length];
+            Parallel.For(0, imageData.Length, i => allPixelValues[i] = imageData[i] / 255.0);
+
+            Parallel.For(0, dataSetSize, imageIndex =>
+            {
+                int byteOffset = imageIndex * bytesPerImage;
+                double[] pixelValues = new double[bytesPerImage];
+                Array.Copy(allPixelValues, byteOffset, pixelValues, 0, bytesPerImage);
+
+                images[imageIndex] = new Image(imageSize, greyscale, pixelValues, labelData[imageIndex]);
+            });
+
+            return images;
+        }
+
+        [Serializable]
         public struct DataFile
         {
             public string imageFilePath;
